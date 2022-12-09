6decb82eacaf930e74c46f3bc8effaecded2caee
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-08 17:33:42 -0800

    sizeof 0.2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %i byte(s)\n", (int) sizeof(a));
 	printf("Size of an int: %i byte(s)\n", (int) sizeof(b));
 	printf("Size of a long int: %i byte(s)\n", (int) sizeof(c));
 	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(d));
 	printf("Size of a float: %i byte(s)\n", (int) sizeof(e));
+
+	return (0);
 }
