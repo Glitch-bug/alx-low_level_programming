844d1a22069e94bbb5e28f04df21a91117ce500d
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-08 16:02:39 -0800

    multi 0.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* main function prints statement*/
+/**
+ * main - function prints statement
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
