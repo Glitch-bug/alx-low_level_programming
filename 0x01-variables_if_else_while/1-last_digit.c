ef44473ad26262dc7203020530c2b7c7fb73e8b6
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-11 10:38:01 -0800

    last of out number 0.1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,6 +18,27 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	sprintf(buffer, "%d", n);
 	ch = strlen(buffer);
-	printf("%c\n", buffer[ch - 1]);
+
+	char lnum = buffer[ch - 1];
+
+	if (n < 0)
+	{
+		printf("Last digit of %i is -%c and is less than 6 and not 0\n",
+		       n, lnum);
+	}
+	else if ((int) lnum == '0')
+	{
+		printf("Last digit of %i is %c and is 0\n", n, lnum);
+	}
+	else if ((int) lnum < '6')
+	{
+		printf("Last digit of %i is %c and is less than 6 and not 0\n",
+		       n, lnum);
+	}
+	else
+	{
+		printf("Last digit of %i is %c and is greater than 5\n",
+		       n, lnum);
+	}
 	return (0);
 }
