9af9c09cd0225453c3d2aaede562c192d020e32d
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-12 03:10:39 -0800

    Hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - prints single digit base 10 numbers
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int nums[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	char abc[6] = "abcdef";
+	for (i = 0; i < 10; i++)
+	{
+		putchar(letrs[i] % 10 + '0');
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(abc[i]);
+	}
+	putchar('\n');
+	return (0);
+}
