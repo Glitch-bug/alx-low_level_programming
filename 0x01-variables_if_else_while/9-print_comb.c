db9757c58c3d8b7f2a4ebe9998e86bb314732918
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-12 03:55:59 -0800

    9-print_comb.c 0.2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+0;10;1c0;10;1c#include <stdio.h>
 
 /**
  *main - prints every combinaation of single digit base 10 numbers
@@ -8,23 +8,13 @@
 int main(void)
 {
 	int i;
-	int j;
-	int m;
-	/**
-	 *int first = 0;
-	 *int f_space = 0;
-	 *int space = 1;
-	 */
 	int letrs[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	for (i = 0; i < 10; i++)
 	{
-		j = i+1;
-		for (m = 0; m < j; m++)
-		{
-			putchar(letrs[m] % 10 + '0');
-		}
-		putchar('\n');
+		putchar(letrs[i] % 10 + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
