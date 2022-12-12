6b0c4c36223742ff3f35e1fea0675930559880c5
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-12 04:48:22 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,7 @@
 
 int main(void)
 {
-     if (write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	       59) != 59)
-     {
-	     write(2, "There was an error writing to standard out\n", 44);
-	     return -1;
-     };
-     return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      58);
+	return (0);
 }
