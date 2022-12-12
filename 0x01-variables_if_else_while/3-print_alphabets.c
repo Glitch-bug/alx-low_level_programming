6c6e088d488897dc2d6a26de2c1e841e9fa5cc34
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-11 16:38:26 -0800

    alpha soup'

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet uppercase and lower case
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char letrs[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(letrs[i]);
+	}
+	putchar('\n');
+	return (0);
+}
