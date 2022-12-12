63a883eb5fdd0ad22f020bdf62f8023fc2e1f1ab
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-11 17:07:21 -0800

    counting numberz 0.2

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int i;
-	int letrs[11] = {0,1,2,3,4,5,6,7,8,9};
+	int letrs[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(letrs[i]);
+		putchar(letrs[i]%10+'0');
 	}
 	putchar('\n');
 	return (0);
