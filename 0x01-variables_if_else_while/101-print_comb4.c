364b6a31eee280058c2fd1bf6098dbf56ee8d83e
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-12 13:22:32 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - prints every combinaation of single digit base 10 numbers
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int numrs[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10 ; j++)
+		{
+			for (k = j +1; k < 10; k++)
+			{
+				putchar(numrs[i] % 10 + '0');
+				putchar(numrs[j] % 10 + '0');
+				putchar(numrs[k] % 10 + '0');101-print_comb4.c
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
