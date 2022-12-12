64b9208699bc3a2c7eace264deb4a6aa0477a442
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-11 16:43:56 -0800

    char bet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet lower case without q and e
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char letrs[25] = "abcdfghijklmnoqrstuvwxyz";
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(letrs[i]);
+	}
+	putchar('\n');
+	return (0);
+}
