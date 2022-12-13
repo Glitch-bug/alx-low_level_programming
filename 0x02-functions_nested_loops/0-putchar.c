e606d9d621645fffc9ea57644abd885d18a27ef0
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-13 14:28:58 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  *main - prints _putchar and newline
  *Return: 0
  */
+
 int main(void)
 {
-	printf("_putchar");
+	int i = 0;
+	char mess [90] = "_putchar\n";
+	while(mess[i] != "\0")
+	{
+		putchar(mess[i]);
+	}
 	return (0);
 }
