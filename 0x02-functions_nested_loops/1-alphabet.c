cfbae3135978dcea1e94edd401054b75e8017908
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-13 15:55:23 -0800

    1-alphabet.c 0.1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+0;10;1c0;10;1c#include "main.h"
 
 /**
  *print_alphabet - prints the alphabet in lowercase
@@ -14,5 +14,5 @@ void print_alphabet(void)
 	{
 		_putchar(alph[i]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
