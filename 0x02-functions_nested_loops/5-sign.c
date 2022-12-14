3e6699dae2c80e4c4e1c446516dba92fe77908ec
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-14 15:51:12 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ *print_sign - prints the sign of the number
+ *@n: int input
+ *Returns:1 if positive, 0 if 0 and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar("+");
+		return (1);
+	}
+	else if(n == 0)
+	{
+		putchar("0");
+		return (0);
+	}
+	else
+	{
+		putchar("-");
+		return (-1);
+	}
+}
