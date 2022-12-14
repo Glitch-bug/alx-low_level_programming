d9f149ec4aa59d7d82611224aa093fe97359222f
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-13 16:14:14 -0800

    ten more times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
+	char alph[27] = "abcdefghijklmnopqrstuvwxyz";
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alph[j]);
+		}
+		_putchar('\n');
 	}
 }
