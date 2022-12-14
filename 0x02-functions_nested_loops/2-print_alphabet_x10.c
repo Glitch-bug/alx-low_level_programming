19e8d7b69c47d82e6fcb9604e0b58252e71de402
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-13 16:07:02 -0800

    ten more times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet in lowercase ten times
+ *
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet()
+	}
+}
