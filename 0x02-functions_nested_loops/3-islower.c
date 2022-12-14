8dcaf71a4a64dc1655ecacec9cdc48a97defe2cc
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-14 14:27:06 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+
+/**
+ *main - checks if input is a lowercase character
+ *
+ *Return: 1 if lowercase or 0 if not
+ */
+
+int _islower(int c)
+{
+	int alph [27]= "abcdefghijklmnopqrstuvxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		if (c == alph[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
