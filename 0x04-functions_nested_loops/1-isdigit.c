9f55f67a8bcf8a129ed3a2e9a9e2eca31dadcbd8
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-15 14:17:10 -0800

    isdigit

---
+++ 0x04-functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+/**
+ *_isdigit - checks if input is a digit
+ *@c - expects int and is checked against digits
+ *Return: 1 if c is digit or 0 if c is not digit
+ */
+
+
+
+int _isdigit(int c)
+{
+	int i;
+	int nums[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+		for(i = 0; i < 10; i++)
+		{
+			if(c == nums[i])
+			{
+				return (1);
+			}
+		}
+	return (0);
+}
