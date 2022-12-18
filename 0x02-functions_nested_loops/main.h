289e5e8574274c48398aa9d9fb1036b76cddbe88
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 15:07:03 -0800

    last digits

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -6,3 +5,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int _putchar(char);
+int _putchar(char);
