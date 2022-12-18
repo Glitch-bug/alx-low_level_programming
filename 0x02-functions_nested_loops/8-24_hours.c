b3b29201c9dde3e7493b3065a7e9c17a13f8befd
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 15:36:18 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *jack_bauer - prints every minute of the day, starting from 00:00 to 23:59
  *Return: void
  */
 
@@ -15,10 +15,10 @@ void jack_bauer(void)
 	min = 0;
 	sec = 0;
 
-	while (min < 25)
+	whil e(min < 25)
 	{
 		printf("%02d:%02d", min, sec);
-		if(sec < 61)
+		if (sec < 61)
 		{
 			sec++;
 		}
