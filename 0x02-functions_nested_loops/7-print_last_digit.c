e4488cce5f6cfe54a3e8641f7453628767693205
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 11:31:46 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *print_last_digit - prints last digit of integer input
+ *@c: integer input
+ *Return: last digit
+ */
+
+int print_last_digit(int c)
+{
+	int ch;
+	char buffer[50];
+	char lnum;
+	sprintf(buffer, "%d", c);
+	ch = strlen(buffer);
+	lnum = buffer[ch - 1];
+	putchar('0'+lnum);
+	return (lnum);
+}
