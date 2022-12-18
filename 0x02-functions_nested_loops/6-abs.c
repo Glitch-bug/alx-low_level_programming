df91098ce2f5ada97aadde3cc00be33c6c48beba
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 11:21:23 -0800

    absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ *_abs - computes the absolute value of integer inputs
+ *@c: int input
+ *Return: abs value
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		return (c * -1);
+	else
+		return (c);
+}
