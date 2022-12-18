5e6d471b6a16b2d12951f958dce63e277d44a0df
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 15:14:17 -0800

    _putchar over putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *print_sign - prints the sign of the number
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		putchar('+');
 	}
 	else if(n == 0)
 	{
+		_putchar('0');
 		return (0);
-		putchar('0');
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
-		putchar('-');
 	}
 }
