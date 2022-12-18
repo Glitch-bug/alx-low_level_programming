4db46b0a2c5d47a6fe32f1a46ce993d39ab867a0
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 15:16:02 -0800

    _putchar over putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  *print_sign - prints the sign of the number
  *@n: int input
- *Returns:1 if positive, 0 if 0 and -1 if negative
+ *Return:1 if positive, 0 if 0 and -1 if negative
  */
 
 int print_sign(int n)
@@ -13,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
