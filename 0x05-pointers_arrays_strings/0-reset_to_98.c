278a74aad313f9f6d16ace0012013bb5847c10c8
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-19 15:00:43 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
+/**
+ *reset_to_98 - takes a pointer to and int as a parameter and updates its value 
+ *to 98
+ *@*n: pointer to an int provides address to be updated
+ */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (*n);
 }
