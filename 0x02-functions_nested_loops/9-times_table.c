a7eccc123ef6ef658c89c3474869550700e4ccf3
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 16:00:13 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *times_table - prints the nine times table starting with 0
+ *Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int mult;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			mult = i * j;
+			_printf("%d,",mult);
+		}
+		_printf("\n");
+	}
+}
