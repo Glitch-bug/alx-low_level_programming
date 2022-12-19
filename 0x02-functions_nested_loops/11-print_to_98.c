67843133670a2cc19fb2d3de89ac4f7cf160616d
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-18 16:42:37 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,35 @@
 
 void print_to_98(int n)
 {
-	while (n < 99)
+	if (n < 99)
 	{
-		if (n < 98)
+		while (n < 99)
 		{
-			printf("%d, ", n);
-		}
-		else
-		{
-			printf("%d", n);
+			if (n < 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
 		}
 	}
+	else
+	{
+		while (n < 97)
+		{
+			if (n < 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n--;
+		}
+	}
+
 }
