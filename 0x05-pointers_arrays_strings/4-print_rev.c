3ba32da8b1970c6064e3834861320dfb5818e500
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-20 15:56:31 -0800

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *print_rev - prints string in reverse
+ *@s: string
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+	}
+	_putchar('\n');
+}
