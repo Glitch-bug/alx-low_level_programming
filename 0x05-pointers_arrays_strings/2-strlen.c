5af212fe866adf6a67e1f0833c9d03540adf75bc
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-20 15:15:33 -0800

    long letter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: the string
+ *Return: integer string length
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while( *s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	return (count);
+}
