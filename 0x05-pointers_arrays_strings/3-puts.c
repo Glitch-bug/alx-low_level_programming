c96589e1bbddcd5b70d303dcd77a8e0f80cbb31e
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-20 15:28:52 -0800

    put it out there

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ *_puts - Prints a string with a newline
+ *@str: string to be printed
+ *Return: void
+ */
+
+void _puts(char *str)
+{
+	int len = _strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
+}
