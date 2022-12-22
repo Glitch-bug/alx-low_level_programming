4e628f899fd45cd01bafe08d0320bf2543c6420e
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-22 13:38:05 -0800

    half are fact

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_puts2 - Prints every other character in a string and then a newline
+ *@str: string to be printed
+ *Return: void
+ */
+
+void _puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(*str);
+		str++;
+		str++;
+	}
+	_putchar('\n');
+}
