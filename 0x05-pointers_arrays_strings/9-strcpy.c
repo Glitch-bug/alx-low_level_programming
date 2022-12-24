6f5c7c98a8c3ec7879fddf1ebf3f41c33cbc2ff1
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-24 13:55:13 -0800

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  *_strcpy - copies the string from the source and posts its entirety to dest
@@ -9,6 +10,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	int len;
+	int i;
+
 	dest = src;
+	len = strlen(src);
+
+	for (i = 0, i < len; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
 	return (dest);
 }
