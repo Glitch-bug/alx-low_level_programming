03601e4804e8d868b86241162463c76c8bac6c96
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-24 13:48:01 -0800

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strcpy - copies the string from the source and posts its entirety to dest
+ *@dest: string is copied to here
+ *@src: string is copied from here
+ *Return: the pointer to @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
