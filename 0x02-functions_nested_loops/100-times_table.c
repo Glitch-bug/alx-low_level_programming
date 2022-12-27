305f64d52d4b24e632b17974aa9fcf224be362a1
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-27 06:43:05 -0800

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_times_table - prints the n times table starting with 0
+ *@n: int whose time table will be printed
+ *Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int mult;
+
+	if ((n > 0) && (n < 15))
+	{
+
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				mult = i * j;
+				if (j > 0 && j < n)
+				{
+					printf("% 4d,", mult);
+				}
+				else if (j == n)
+				{
+					printf("% 4d", mult);
+				}
+				else
+				{
+					printf("%d,", mult);
+				}
+			}
+			printf("\n");
+		}
+	}
+}
