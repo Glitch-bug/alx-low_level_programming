4b922593973c6ea571f9c67cd5953654cd20bd15
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-31 15:31:11 -0800

    conc int

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ *_strncat - concactenates two strings
+ *@dest: destination string
+ *@src: additional string
+ *@n: int deciding how much additional string to add
+ *Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i;
+	int f_count;
+	int count;
+
+
+	char hold[1000] = "";
+
+
+	f_count = strlen(dest);
+	count = f_count;
+	for (i = 0; i < f_count; i++)
+	{
+		hold[i] = dest[i];
+	}
+	for (i = count; i <= (count + n); i++)
+	{
+		hold[i] = src[i - count];
+	}
+	memcpy(dest, hold, strlen(hold));
+	return (dest);
+}
