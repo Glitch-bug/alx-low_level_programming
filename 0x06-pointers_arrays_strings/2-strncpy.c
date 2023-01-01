e12fd7807b77d4e799a7deec76527b388a34053a
Glitch-bug <kwabenayiadom271@gmail.com>
2022-12-31 16:07:06 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strncpy - works exactly like strncpy (man strncpy for more information)
+ *@dest: destination that will hold copied string value
+ *@src: source and superset of the copied string
+ *@n: number of chars to be from @src
+ *Return: pointer to @dest
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return (dest);
+}
