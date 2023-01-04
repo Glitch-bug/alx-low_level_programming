a46e0fea21af197bf1781458d13499fe0ca8b056
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-04 15:12:16 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+ *_memcpy - copies @n bytes from @src to @dest
+ *@dest: destination variable pointer
+ *@src: source variable pointer
+ *Return: pointer to @dest
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+		dest++;
+		src++;
+	}
+	return (dest);
+}
