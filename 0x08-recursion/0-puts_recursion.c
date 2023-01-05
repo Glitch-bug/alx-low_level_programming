f262acdf2713e8b53a43b6c75d745170a1cdc23e
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-05 14:18:02 -0800

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include main.h
+/**
+ * _puts_recursion -  prints a string, followed by a new line.
+ *@s: string to be printed
+ */
+
+void _puts_recursion(*s)
+{
+	if (!*s)
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+Recursion when doing the same thing and expecting different results is considered
+smart.
