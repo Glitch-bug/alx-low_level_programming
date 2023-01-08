7eecc0a30790ff8119ef6500eaaf50105944ad22
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-07 17:20:26 -0800

    print_rev_rec

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,16 +8,12 @@
 
 void _print_rev_recursion(char *s)
 {
-
-	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-	}
-
 	if (*s == '\0')
 	{
 		return;
 	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
 	_putchar(*s);
 }
