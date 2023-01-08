adeaa3c4c5e58a12eb5ccfaa2296cd5489bb80e0
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-07 17:58:10 -0800

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: this int will be raised to the power @y
+ * @y: @x will be raised to the power @y
+ * Return: @x raised to the power @y
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(y - 1));
+}
