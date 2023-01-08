3c7a328e567d308cafcabe6595d936bad9ed4870
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-07 16:57:54 -0800

    print_rev_rec

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string
+ */
+
+void _print_rev_recursion(*s)
+{
+	int i;
+
+	for(i = strlen(s); i => 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
