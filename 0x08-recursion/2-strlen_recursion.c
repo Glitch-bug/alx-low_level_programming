4bd4ba7d7f6a7319ea2d973ace47ceab75ac88c1
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-07 17:28:40 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+/**
+ *_strlen_recursion(char *s): returns the length of a string.
+ *@s: the string
+ *Return: the length of the string
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return 0;
+	}
+	s++;
+	return 1 + _strlen_recursion(s);
+}
