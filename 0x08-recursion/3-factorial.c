72dc7f817b59bb98a221612117b16437a29d6c9d
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-07 17:44:10 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,14 @@
 /**
  * factorial - returns the factorial of a given number
  * @n: the given number
- *Returns: @n factorial
+ * Return: @n factorial
  */
 
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n ==1)
+	else if (n == 0 || n == 1)
 		return (1);
 
 	return (n * factorial(n - 1));
