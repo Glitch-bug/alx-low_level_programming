0f499c02a4975651c417f2b45c8ef883d7ca5ce1
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-11 15:35:13 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by a new line
+ * @argc: number of arguements entered
+ * @argv: array  of arguement pointers
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
