3e07589ed98b1ed77bda28229bc9154e1e3bf7bb
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-14 15:49:36 -0800

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints the multiple of two arguements
+ *@argc: number of arguements
+ *@argv: array of arguements
+ *Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
