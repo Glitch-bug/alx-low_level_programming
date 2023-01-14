55f2f0aafae1fe62cf635e7b589ab5e3c2544e2b
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-14 15:19:19 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguements
+ *@argc: int represent number of arguements
+ *@argv: array of pointers
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%d", argc - 1);
+}
