b55266672f3b1d899106691615634042cc9af06f
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-14 16:50:02 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,17 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int len;
-	int sum;
+	int sum = 0;
 	int j;
 
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			len = strlen(argv[i]);
-			for (j = 0; j < len; i++)
+			for (j = 0; j < len; j++)
 			{
-				if ((argv[i][j] >= '0' && argv[i][j] <= '9'))
-				{
-					continue;
-				}
-				else
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 				{
 					printf("Error\n");
 					return (1);
@@ -35,7 +31,7 @@ int main(int argc, char *argv[])
 			}
 			sum += atoi(argv[i]);
 		}
-		printf("%d", sum);
+		printf("%d\n", sum);
 		return (0);
 	}
 	printf("0\n");
