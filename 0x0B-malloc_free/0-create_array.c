eb2cc8858062987e6729f1ce374b6ca0fa1e1dfd
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-20 16:00:03 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *r = malloc(sizeof(char) * size);
+	char *r = (char*)malloc(sizeof(char) * size);
 	unsigned int i = 0;
 
 	if (size == 0)
