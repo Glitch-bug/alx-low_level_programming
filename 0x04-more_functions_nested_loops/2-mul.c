eb8358cab2e78478ab0cadc8e7ccb30b848fc9dd
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-29 10:52:05 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - Write a function that multiplies two integers.
+ *@a: first integer
+ *@b: second integer
+ *Return: mutiple of @a and @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
 int _putchar(char);
