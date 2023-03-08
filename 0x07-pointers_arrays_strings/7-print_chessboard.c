40f34ef2aff65eb742e0ce122a911fe394e708d8
Glitch-bug <kwabenayiadom271@gmail.com>
2023-03-08 15:30:18 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *print_chessboard - a function that prints the chessboard.
+ *@board: list of list representing chessboard
+ */
+
+void print_chessboard(char board[][8])
+{
+	int count = 8;
+	int i = 0;
+	int end = strlen(*board);
+
+	while (i < end)
+        {
+		if((i != 0) && (i % count == 0))
+		{
+			_putchar('\n');
+		}
+		_putchar(*(*board + i));
+		i++;
+	}
+	_putchar('\n');
+}
