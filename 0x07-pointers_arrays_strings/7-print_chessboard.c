958d803dfec72d4ba0e4b68ef26b5273ce2dd346
Glitch-bug <kwabenayiadom271@gmail.com>
2023-03-11 15:56:44 -0800

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,7 +10,7 @@ void print_chessboard(char board[][8])
 {
 	int i = 0;
 
-	while (((*(*board + i)) > 65 || (*(*board + i)) == 32)
+	while (((*(*board + i)) > 64 || (*(*board + i)) == 32)
 	       && (*(*board + i) < 123))
 	{
 		if (i != 0 && (i % 8 == 0))
