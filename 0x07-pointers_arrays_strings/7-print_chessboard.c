816a75fc32c989c04357736151bdb43545f3fc15
Glitch-bug <kwabenayiadom271@gmail.com>
2023-03-11 15:15:46 -0800

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
+// Dear future me count the capital R's end after last one you go mate
+// Should that fail create a single string and divide its length by eight
 /**
  *print_chessboard - a function that prints the chessboard.
  *@board: list of list representing chessboard
@@ -9,18 +10,16 @@
 
 void print_chessboard(char board[][8])
 {
-	int count = 8;
 	int i = 0;
-	int end = strlen(*board);
+	int count = 0;
 
-	while (i < end)
+	while (count < 2)
 	{
-		if ((i != 0) && (i % count == 0))
-		{
+		if (i != 0 && ((*(*board + i) % 8) == 0))
 			_putchar('\n');
-		}
+		else if((*(*board + i)) == 'R')
+			count += 1;
 		_putchar(*(*board + i));
-		i++;
+ 		i++;
 	}
-	_putchar('\n');
 }
