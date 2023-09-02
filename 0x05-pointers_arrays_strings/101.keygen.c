eb61c2bee9623076e948bc142bb76eb9e3e6faf5
Glitch-bug <kwabenayiadom271@gmail.com>
2023-09-02 23:45:18 +0000

    keygen progress

--- 0x05-pointers_arrays_strings/101.keygen.c
+++ 0x05-pointers_arrays_strings/101.keygen.c
@@ -4,9 +4,38 @@
 int select(int min, int max);
 char* main()
 {
+	char perm[] = {'L', 'U', 'S', 'N'};
+	char state[] = {'L', 'U', 'S', 'N'};
+	char pass[16];
+	int c;
+	int i;
+	int length;
 
-        srand(time(NULL));
-	printf("fuck?%d", r);
+
+	srand(time(NULL));
+	length = select(8, 15);
+	pass[length] = '\0';
+	for (i = 0; i < length; i++)
+	{
+		c = select(0, 3);
+		if (perm[c] == state[c])
+		{
+			if (perm[c] == 'L')
+			{
+			} else if (perm[c] == 'U')
+			{
+				
+				perm[c] = 'u';
+			} else if (perm[c] == 'S')
+			{
+				perm[c] = 's';
+			} else if (perm[c] = 'N')
+			{
+				perm[c] = 'n';
+			}
+		}
+	}
+	printf("fuck?");
         return "fuck";
 }
 
@@ -17,4 +46,5 @@ int select(int min, int max)
 	{
 		r = rand(2) % max + 1;
 	}
+	return r;
 }
