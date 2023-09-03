61b849469a093c1e284830d2595f8f1d0f96bd0b
Glitch-bug <kwabenayiadom271@gmail.com>
2023-09-03 00:47:32 +0000

    almost

--- 0x05-pointers_arrays_strings/101.keygen.c
+++ 0x05-pointers_arrays_strings/101.keygen.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-int select(int min, int max);
+int select_r(int, int);
 char* main()
 {
 	char perm[] = {'L', 'U', 'S', 'N'};
@@ -11,40 +12,44 @@ char* main()
 	int i;
 	int length;
 
-
 	srand(time(NULL));
-	length = select(8, 15);
+	length = select_r(8, 15);
 	pass[length] = '\0';
-	for (i = 0; i < length; i++)
+	for (i = 0; i < length;)
 	{
-		c = select(0, 3);
+		c = select_r(1, 4) - 1;
+		printf("%d\n", c);
 		if (perm[c] == state[c])
 		{
 			if (perm[c] == 'L')
 			{
+				pass[i] = select_r(97, 122);
 			} else if (perm[c] == 'U')
 			{
-				
+				pass[i] = select_r(65,90);
 				perm[c] = 'u';
 			} else if (perm[c] == 'S')
 			{
+				pass[i] = select_r(33,47);
 				perm[c] = 's';
-			} else if (perm[c] = 'N')
+			} else if (perm[c] == 'N')
 			{
+				pass[i] = select_r(48,57);
 				perm[c] = 'n';
 			}
+			i++;
 		}
 	}
-	printf("fuck?");
+	printf("%s",pass);
         return "fuck";
 }
 
-int select(int min, int max)
+int select_r(int min, int max)
 {
-        int r = 0;
+        int r = -1;
 	while (r < min)
 	{
-		r = rand(2) % max + 1;
+		r = rand() % (max + 1);
 	}
 	return r;
 }
